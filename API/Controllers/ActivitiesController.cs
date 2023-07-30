4ccf4a48372a8e6f34dad1a8647526a7a7733162
unknown <miguel_mendieta_50@hotmail.com>
2023-07-29 22:00:45 -0400

    End of section 3b

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,7 +21,6 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-
             return await _context.Activities.FindAsync(id);
         }
     }
